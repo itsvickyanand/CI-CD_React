b12a9a195599f7392123ff68cefea40113670be4
itsvickyanand <vickyanandtoprock@gmail.com>
2023-01-08 20:25:10 +0530

    actions

--- githubactions/src/App.js
+++ githubactions/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -10,17 +10,16 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React fdfdffd
+          Learn React fdfdffdfddffd
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
